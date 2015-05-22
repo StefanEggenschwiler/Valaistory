8ed7482d288fc0a125ccacfa9e10882169d47173
maniac1989 <pierry_wyssen@hotmail.com>
2015-05-22 18:43:50 +0200

    removed unused code

--- app/src/main/java/hevs/ch/valaistory/TestParseXMLActivity.java
+++ app/src/main/java/hevs/ch/valaistory/TestParseXMLActivity.java
@@ -33,26 +33,4 @@ public class TestParseXMLActivity extends ActionBarActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(itemsAdapter);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_test_parse_xml, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
